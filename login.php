8664e95d7bd607e8fe7e96e2024d054d75eebdec
vostrak318 <vostrak318@seznam.cz>
2023-04-19 12:29:27 +0200

    login a style pro login

---
+++ login.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="loginStyle.css">
+    <script src="https://kit.fontawesome.com/abc1fc3e2f.js" crossorigin="anonymous"></script>
+</head>
+<body>
+<div class="white-square">
+        <div class="h1">
+            Login
+        </div>
+        <div class="username">
+            Username
+        </div>
+        <div class="jmeno">
+            <label for="jmeno"></label>
+            <input type="text" id="jmeno" name="jmeno" placeholder="Enter your username">
+        </div>
+        <div class="password">
+            Password
+        </div>
+        <div class="heslo">
+            <label for="heslo"></label>
+            <input type="password" id="heslo" name="heslo" placeholder="Enter your password" >
+        </div>
+        <div class="submit">
+            <input id="submitedit" type="submit" value="Log in">
+        </div>
+        <div class="p1">
+            Or sign in with:
+        </div>
+        <div>
+            <a href="https://cs-cz.facebook.com/"><i class="fab fa-facebook" style="font-size: 60px; margin-top: 10%; margin-left: 17%; color: rgb(51, 51, 156);"></i></a>
+            <a href="https://twitter.com/i/flow/login?input_flow_data=%7B%22requested_variant%22%3A%22eyJyZWRpcmVjdF9hZnRlcl9sb2dpbiI6Imh0dHBzOi8vZGV2ZWxvcGVyLnR3aXR0ZXIuY29tL2VuL2FwcHMifQ%3D%3D%22%7D"><i class="fab fa-twitter" style="font-size: 60px; margin-top: -30%; margin-left: 9%; color: white; background-color: rgb(106, 106, 216); border-radius: 10px;"></i></a>
+            <a href="https://www.google.cz/"><i class="fab fa-google-plus" style="color: tomato; background-color: white; font-size: 60px; margin-left: 9%;"></i></a>
+        </div>
+    </div>
+</body>
+</html>
