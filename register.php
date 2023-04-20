029ffb85ec819d7586db6707afe3814da42dd907
vostrak318 <vostrak318@seznam.cz>
2023-04-20 12:08:55 +0200

    login a register update

--- register.php
+++ register.php
@@ -5,8 +5,41 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register</title>
+    <link rel="stylesheet" href="registerStyle.css">
+    <script src="https://kit.fontawesome.com/abc1fc3e2f.js" crossorigin="anonymous"></script>
 </head>
-<body>
-    
+<body class="background">
+<div class="white-square">
+        <div class="h1">
+            Register
+        </div>
+        <div class="username">
+            Username
+        </div>
+        <div class="jmeno">
+            <label for="jmeno"></label>
+            <input type="text" id="jmeno" name="jmeno" placeholder="Enter your username">
+        </div>
+        <div class="password">
+            Password
+        </div>
+        <div class="heslo">
+            <label for="heslo"></label>
+            <input type="password" id="heslo" name="heslo" placeholder="Enter your password" >
+        </div>
+        <div class="confirmPassword">
+            Confirm password
+        </div>
+        <div class="potvrdHeslo">
+            <label for="potvrd heslo"></label>
+            <input type="password" id="potvrdHeslo" name="potvrdHeslo" placeholder="Confirm your password" >
+        </div>
+        <div class="submit">
+            <input id="submitedit" type="submit" value="Register">
+        </div>
+        <div class="p1">
+            <a href="http://localhost/workspace/Pexeso/login.php">Login</a>
+        </div>
+    </div>
 </body>
 </html>
