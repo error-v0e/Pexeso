35d67dcacf5f7469dcb8ffad62521e4fcbc2d0e2
vostrak318 <vostrak318@seznam.cz>
2023-04-28 11:55:41 +0200

    prepsani

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
         <input type="number" name="width" min="3" max="10" placeholder="3-10">
         <input class="button" type="submit" value="Start">
     </form>
-    <div id="score">Score: 0</div>
+    <div id="score">Tries: 0</div>
     <table id="table"></table>
 
 
